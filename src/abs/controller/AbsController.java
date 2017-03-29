bbdee701e4693a610a0e88597161f93454f60f83
smeth194243 <smor7432@740s-pl-106.local>
2017-03-29 13:48:35 -0600

    :D Created

---
+++ src/abs/controller/AbsController.java
@@ -0,0 +1,11 @@
+package abs.controller;
+
+import abs.model.*;
+
+public class AbsController 
+{
+	public void start()
+	{
+		
+	}
+}
---
+++ src/abs/controller/Runner.java
@@ -0,0 +1,10 @@
+package abs.controller;
+
+public class Runner 
+{
+	public static void main(String []args)
+	{
+		AbsController absAppController = new AbsController();
+		absAppController.start();
+	}
+}
